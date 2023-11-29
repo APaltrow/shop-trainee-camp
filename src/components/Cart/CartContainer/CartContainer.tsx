4b90d29f9a3dcb7f92aba244e452e21fc28d36b6
Paltrow <alexandriys2@ukr.net>
2023-11-29 19:43:46 +0200

    Added ui and layout for Order summary section.

--- src/components/Cart/CartContainer/CartContainer.tsx
+++ src/components/Cart/CartContainer/CartContainer.tsx
@@ -6,11 +6,18 @@ import {
   ButtonVariants,
   ButtonSizes,
   TYPE_CHECKBOX,
+  IconsTypes,
 } from '@constants';
 import { BillingInputProps, FormInputEvent } from '@types';
 import { useAddressAutocomplete, useValidations } from '@hooks';
 import { useActions, useAppSelector } from '@redux';
-import { CustomButton } from '@components';
+import {
+  CustomButton,
+  CustomImage,
+  CustomInput,
+  Icon,
+  Rating,
+} from '@components';
 
 import { CartBillingInfo } from '../CartBillingInfo';
 import { CartAdditionalInfo } from '../CartAdditionalInfo';
@@ -127,7 +134,98 @@ export const Cart: FC = () => {
       </div>
 
       <section className={style.order}>
-        <h2>Cart lots here</h2>
+        <div>
+          <h2 className={style.order_title}>Order summary</h2>
+          <p className={style.order_text}>
+            Price can change depending on shipping method and taxes of your
+            state.
+          </p>
+        </div>
+        <ul className={style.order_list}>
+          {[...new Array(3)].map((_, idx) => (
+            <li key={idx}>
+              <article className={style.order_item}>
+                <div className={style.order_item_left}>
+                  <div className={style.order_item_img}>
+                    <CustomImage
+                      fullSize
+                      src="https://images.pexels.com/photos/15469650/pexels-photo-15469650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    />
+                  </div>
+
+                  <CustomButton>
+                    <span className={style.order_item_icon_heart}>
+                      <Icon iconName={IconsTypes.HEART} />
+                    </span>
+
+                    <span className={style.order_item_btn}>Wishlist</span>
+                  </CustomButton>
+
+                  <CustomButton>
+                    <Icon iconName={IconsTypes.CLOSE} />
+                    <span className={style.order_item_btn}>Remove</span>
+                  </CustomButton>
+                </div>
+                <div className={style.order_item_right}>
+                  <div className={style.order_item_description}>
+                    <h3 className={style.order_item_title}>Product Title</h3>
+                    <ul className={style.order_item_list}>
+                      <li>
+                        <span>Farm: </span>
+                        <span>Tharamis Farm</span>
+                      </li>
+                      <li>
+                        <span>Freshness: </span>
+                        <span>1 day old</span>
+                      </li>
+                    </ul>
+                    <Rating
+                      rating={4}
+                      isActive
+                    />
+                  </div>
+
+                  <div className={style.order_item_price_section}>
+                    <p className={style.order_item_price}>36.99 USD</p>
+                    <span>SELECT here</span>
+                  </div>
+                </div>
+              </article>
+            </li>
+          ))}
+        </ul>
+
+        <div className={style.order_tax}>
+          <div className={style.order_sub_total}>
+            <p className={style.order_sub_total_item}>
+              <span>Subtotal</span>
+              <span>73.98 USD</span>
+            </p>
+            <p className={style.order_sub_total_item}>
+              <span>Tax</span>
+              <span>17% 16.53 USD</span>
+            </p>
+          </div>
+
+          <div className={style.promo_code}>
+            <CustomInput placeholder="Apply promo code" />
+
+            <span className={style.apply_btn}>
+              <CustomButton onClick={() => {}}>Apply now</CustomButton>
+            </span>
+          </div>
+        </div>
+
+        <div className={style.order_total}>
+          <div>
+            <p className={style.order_total_title}>Total Order</p>
+            <p className={style.order_total_delivery}>
+              Guaranteed delivery day: June 12, 2020
+            </p>
+          </div>
+
+          <p className={style.order_total_price}>89.84 USD</p>
+        </div>
       </section>
     </form>
   );
