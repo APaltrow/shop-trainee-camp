5cb1570a485803efa2498cd4c8278cb25ac81e64
Paltrow <alexandriys2@ukr.net>
2023-12-05 13:40:25 +0200

    fix: Fixed Alert overflow and style to be displayed on center. Optimized useNoScroll hook.

--- src/hooks/useNoScroll.ts
+++ src/hooks/useNoScroll.ts
@@ -1,26 +1,19 @@
 import { useEffect } from 'react';
-import { MOUSEWHEEL_EVENT, SCROLL_EVENT, TOUCHMOVE_EVENT } from '@constants';
 
 export const useNoScroll = (isNoScroll: boolean) => {
   useEffect(() => {
     const pos = Math.trunc(window.scrollY);
 
     const disableScroll = () => {
-      if (!isNoScroll) return;
-
       window.scrollTo({
         top: pos,
       });
     };
 
-    document.addEventListener(SCROLL_EVENT, disableScroll, false);
-    document.addEventListener(MOUSEWHEEL_EVENT, disableScroll, false);
-    document.addEventListener(TOUCHMOVE_EVENT, disableScroll, false);
+    window.onscroll = isNoScroll ? disableScroll : () => {};
 
     return () => {
-      document.removeEventListener(SCROLL_EVENT, disableScroll);
-      document.removeEventListener(MOUSEWHEEL_EVENT, disableScroll);
-      document.removeEventListener(TOUCHMOVE_EVENT, disableScroll);
+      window.onscroll = () => {};
     };
   }, [isNoScroll]);
 };
