676a1239f2c99f1335cd4411a4f2c60b90d86c24
Paltrow <alexandriys2@ukr.net>
2023-12-11 12:29:15 +0200

    Added pagination to wish list.

--- src/components/Wishlist/WishlistProducts/WishlistProducts.tsx
+++ src/components/Wishlist/WishlistProducts/WishlistProducts.tsx
@@ -1,7 +1,10 @@
 import { FC } from 'react';
 
 import { IProduct } from '@types';
-import { ProductsItem } from '@components';
+import { getPaginatedList } from '@helpers';
+import { usePagination } from '@hooks';
+import { ButtonSizes, ButtonVariants, IconsTypes } from '@constants';
+import { CustomButton, Icon, Pagination, ProductsItem } from '@components';
 
 import { EmptyWishlist } from '../EmptyWishlist';
 
@@ -14,13 +17,47 @@ interface WishlistProductsProps {
 export const WishlistProducts: FC<WishlistProductsProps> = ({ list }) => {
   if (!list.length) return <EmptyWishlist />;
 
+  const {
+    pagesList,
+    activePage,
+    isShowMoreVisible,
+
+    onShowMore,
+    onActivePageChange,
+  } = usePagination(list.length, [list]);
+
+  const paginatedList = getPaginatedList(pagesList, list, activePage);
+
   return (
-    <ul className={style.wishlist_container}>
-      {list.map((product) => (
-        <li key={product.productId}>
-          <ProductsItem product={product} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className={style.wishlist_container}>
+        {paginatedList.map((product) => (
+          <li key={product.productId}>
+            <ProductsItem product={product} />
+          </li>
+        ))}
+      </ul>
+      <div className={style.pagination_container}>
+        {!!pagesList.length && (
+          <Pagination
+            pagesList={pagesList}
+            activePage={activePage}
+            onActivePageChange={onActivePageChange}
+          />
+        )}
+        <div className={style.show_more_btn}>
+          {isShowMoreVisible && (
+            <CustomButton
+              onClick={() => onShowMore(activePage)}
+              variant={ButtonVariants.PRIMARY}
+              size={ButtonSizes.MID}
+            >
+              Show more products
+              <Icon iconName={IconsTypes.ARROW_DOWN} />
+            </CustomButton>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
